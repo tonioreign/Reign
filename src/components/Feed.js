9db44257c9014a5ceb51760bf425ab791cb5aa73
tonioreign <tonioreign@gmail.com>
2022-05-10 19:54:58 -0700

    First commit

---
+++ src/components/Feed.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Feed() {
+  return (
+    <div>
+      {/* Header */}
+      <h2>Home</h2>
+      {/* Tweet */}
+
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Feed;
